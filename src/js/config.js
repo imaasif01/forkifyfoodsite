a9a6a950bf11f15fe7f8141aa142c59bdfa7ab96
imaasif <aasifzayd01@gmail.com>
2022-01-02 21:11:11 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = 'baaa8526-5076-41d6-a1bd-4c440b649037';
+export const MODEL_CLOSE_SEC = 2.5;
